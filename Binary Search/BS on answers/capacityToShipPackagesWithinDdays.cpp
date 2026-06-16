6e0aa5de372702896b1b8758f744fecd55df0f40
ht-harshittaneja <ht.harshittaneja@gmail.com>
2026-06-16 16:09:48 +0530

    code+approach

---
+++ Binary Search/BS on answers/capacityToShipPackagesWithinDdays.cpp
@@ -0,0 +1,54 @@
+/* 
+leetcode 1011. Capacity To Ship Packages Within D Days
+A conveyor belt has packages that must be shipped from one port to another within days days.
+
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+ */
+
+/* 
+Optimal Approach:
+Minimum capacity = largest package.
+Maximum capacity = sum of all packages.
+Pick a capacity.
+Compute how many days are needed.
+If days needed ≤ target days → capacity works, try smaller.
+Otherwise → capacity too small, try larger.
+ */
+
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+    int lo = weights[0];
+    int hi = 0 ;
+    for (int x : weights) {
+    lo = max(lo, x);
+    hi += x;
+    }
+    int ans = -1 ;
+    while(lo<=hi){
+        int mid = lo + (hi-lo)/2;
+        int totalDays = noOfDays(weights,mid);
+        if(totalDays <= days){
+            ans = mid;
+            hi = mid -1 ;
+        }
+        else lo =mid +1 ;
+    }
+    return ans;         
+    }
+
+    int noOfDays(vector<int>& weights , int mid){
+    int totalDays = 1 ;
+    int sum =0 ;
+    for(int x : weights){
+        if(sum + x >mid){
+            totalDays++;
+            sum =0;
+        }
+        sum +=x;
+    }   
+    return totalDays;
+    }
+};
--- Binary Search/BS on answers/findTheSmallestDivisor.cpp
+++ Binary Search/BS on answers/findTheSmallestDivisor.cpp
@@ -1,5 +1,5 @@
 /* 
-1283. Find the Smallest Divisor Given a Threshold
+Leetcode : 1283. Find the Smallest Divisor Given a Threshold
 Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
 
 Each result of the division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
@@ -9,6 +9,7 @@ The test cases are generated so that there will be an answer.
 
 /* 
 Optimal Approach: O(n*log(max))
+same as Koko Eating Bananas
 Answer range: divisor from 1 to max element.
 Pick a divisor and compute the sum of all ceil(num / divisor).
 Check validity:
